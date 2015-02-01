374b3440bad4a785a790213befaffa6505954f33
ivanhefe <ivanheras10@gmail.com>
2015-02-01 20:14:47 +0100

    Eventos añadidos
    Cambiados rectangles por canvas, cambia el color al otro grid

--- IvanProyecto/IvanProyecto/MainWindow.xaml.cs
+++ IvanProyecto/IvanProyecto/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,46 +20,66 @@ namespace IvanProyecto {
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        private string p;
-        //IPAddress host = 
         public MainWindow() {
-
+            InitializeComponent();
         }
 
+        //sockets
+        Socket sockCli;
+        IPEndPoint localEndPoint;
+
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            Rectangle re, re1;
+            Canvas ca;
             Color col = Color.FromRgb(41, 40, 43);
+            Thickness margen = new Thickness(1);
             grid.Background = new SolidColorBrush(col);
             gridPropio.Background = new SolidColorBrush(Colors.LightBlue);
             for (int i = 0; i < grid.ColumnDefinitions.Count; i++) {
                 for (int j = 0; j < grid.RowDefinitions.Count; j++) {
-                    re = new Rectangle();
-                    Grid.SetColumn(re, i);
-                    Grid.SetRow(re, j);
-                   
-                    re.Fill = new SolidColorBrush(col);
-                    re.Stroke = new SolidColorBrush(Colors.Black);
-                    re.MouseDown += new MouseButtonEventHandler(Rectangle_MouseDown_1);
-                    grid.Children.Add(re);
+                    ca = new Canvas();
+                    Grid.SetColumn(ca, i);
+                    Grid.SetRow(ca, j);
+                    ca.Margin = margen;
+                    ca.Background = new SolidColorBrush(Colors.Azure);
+                    //ca.Fill = new SolidColorBrush(col);
+                    
+                    ca.MouseDown += new MouseButtonEventHandler(Rectangle_MouseDown_1);
+                    grid.Children.Add(ca);
+                }
+            }
+            //drag and drop
+            //https://msdn.microsoft.com/en-us/library/ms742859%28v=vs.110%29.aspx
+            
+            for (int i = 0; i < grid.ColumnDefinitions.Count; i++) {
+                for (int j = 0; j < grid.RowDefinitions.Count; j++) {
+                    ca = new Canvas();
+                    Grid.SetColumn(ca, i);
+                    Grid.SetRow(ca, j);
+                    ca.Margin = margen;
+                    ca.Background = new SolidColorBrush(Colors.Azure);
+                    //ca.Fill = new SolidColorBrush(col);                  
+                    //ca.MouseDown += new MouseButtonEventHandler(Rectangle_MouseDown_1);
+                    gridPropio.Children.Add(ca);
                 }
             }
 
-            //for (int i = 0; i < gridPropio.ColumnDefinitions.Count; i++) {
-            //    for (int j = 0; j < gridPropio.RowDefinitions.Count; j++) {
-            //        re = new Rectangle();
-            //        Grid.SetColumn(re, i);
-            //        Grid.SetRow(re, j);
-            //        re.Fill = new SolidColorBrush(Colors.LightBlue);
-            //        re.Stroke = new SolidColorBrush(Colors.Black);
-            //        re.MouseDown += new MouseButtonEventHandler(Rectangle_MouseDown_1);
-            //        gridPropio.Children.Add(re);
-            //    }
-            //}
-        }
-
-        public MainWindow(string p) {
-            InitializeComponent();
-            this.p = p;
+            //sockets
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            localEndPoint = new IPEndPoint(ipAddress, 11000);
+            sockCli = new Socket(AddressFamily.InterNetwork,
+                     SocketType.Stream, ProtocolType.Tcp);
+            try {
+                //sockCli.Connect(localEndPoint);
+                //Convert.ToBase64CharArray("prueba2<EOF>");
+                //sockCli.Connect(localEndPoint);
+                
+                //sockCli.Shutdown(SocketShutdown.Both);
+                //sockCli.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
@@ -80,25 +101,51 @@ namespace IvanProyecto {
 
         }
 
+        //devuelve columna y fila
         private void Rectangle_MouseDown_1(object sender, MouseButtonEventArgs e) {
-            Rectangle re = (Rectangle)sender;
-            int x = Grid.GetColumn(re);
-            int y = Grid.GetRow(re);
-            re.Fill = new SolidColorBrush(Colors.Red);
+            
+            Canvas can = sender as Canvas;
+            if (can.Tag == null) {
+                int x = Grid.GetColumn(can);
+                int y = Grid.GetRow(can);
+                can.Background = new SolidColorBrush(Colors.Red);
+                can.Tag = new object();
+                //mandar las coordenadas al otro jugador
 
-            //MessageBox.Show(grid.ColumnDefinitions.Count.ToString());
-            MessageBox.Show(x + "," + y);
+                //MessageBox.Show(x + "," + y);
+
+                //recibir coordenadas
+                foreach (FrameworkElement canv in this.gridPropio.Children) {
+                    if (x == Grid.GetColumn(canv) && y == Grid.GetRow(canv)) {
+                        ((Canvas)canv).Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    }
+                }
+            }
+            else {
+                MessageBox.Show("Ya has clickeado en esta casilla");
+            }
         }
 
+        private void enviarComando(String comando) {
+            byte[] msg = Encoding.ASCII.GetBytes("mensaje prueba<EOF>");
+            try {
+                sockCli.Connect(localEndPoint); 
+                sockCli.Send(msg);
+                sockCli.Shutdown(SocketShutdown.Both);
+                sockCli.Disconnect(true);
+            }
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+            //sockCli.Shutdown(SocketShutdown.Both);
+            
+            
+        }
         private void CheckBox_Checked(object sender, RoutedEventArgs e) {
-            //ventana arriba del todo
-            //CheckBox cb =(CheckBox) sender;
-            //if (cb.IsChecked == true) {
-            //    ventana.Topmost = true;
-            //}
-            //else {
-            //    ventana.Topmost = false;
-            //}
+         
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
@@ -111,9 +158,10 @@ namespace IvanProyecto {
             this.barcos.Visibility = Visibility.Collapsed;
         }
 
+        #region draganddrop no va
         private void Label_MouseMove(object sender, MouseEventArgs e) {
             Label la = sender as Label;
-            if (la!= null && e.LeftButton== MouseButtonState.Pressed) {
+            if (la != null && e.LeftButton == MouseButtonState.Pressed) {
                 DragDrop.DoDragDrop(la, la.Content.ToString(), DragDropEffects.Move);
             }
         }
@@ -124,5 +172,36 @@ namespace IvanProyecto {
                 DragDrop.DoDragDrop(ellipse, ellipse.Fill.ToString(), DragDropEffects.Copy);
             }
         }
+
+        private void gridPropio_Drop(object sender, DragEventArgs e) {
+           
+            
+        }
+
+        private Brush _previousFill = null;
+        private void gridPropio_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap)) {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void gridPropio_DragOver(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.None;
+
+            // If the DataObject contains string data, extract it. 
+            if (e.Data.GetDataPresent(DataFormats.StringFormat)) {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, allow copying.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString)) {
+                    e.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+                }
+            }
+        }
+        #endregion
     }
 }
